7fce6fa87521d8ff46682e30080e0df950a8d064
Kate Dubitski <kate.dubitski@gmail.com>
2021-03-02 15:48:44 -0500

    Add input and button
    -add h1 to display greeting

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ function App() {
   const [greeting] = useState('Hi');
   console.log(greeting);
   return <div className="App">{greeting}</div>;
+  return (
+    <div className="App">
+      <h1>{greeting}</h1>
+      <label>
+        New Greeting:
+        <input
+          type="text"
+          value={inputVal}
+        />
+      </label>
+      <button
+      >
+        Save
+      </button>
+    </div>
+  );
 }
 
 export default App;
