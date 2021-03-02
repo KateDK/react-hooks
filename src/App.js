5118e054ca2d8fe345296286799c76e0f8fc38a4
Kate Dubitski <kate.dubitski@gmail.com>
2021-03-02 16:05:51 -0500

    Refactor component
    -add form tag
    -move handleSubmit to form
    -add button type submit
    -add e.preventDefault to prevent refresh

--- src/App.js
+++ src/App.js
@@ -4,28 +4,24 @@ function App() {
   const [greeting, setGreeting] = useState('Hi');
   const [inputVal, setInputVal] = useState('');
   const handleSubmit = (e) => {
+    e.preventDefault();
     setGreeting(inputVal);
     setInputVal('');
   };
   return (
     <div className="App">
       <h1>{greeting}</h1>
-      <label>
-        New Greeting:
-        <input
-          type="text"
-          value={inputVal}
-          onChange={(e) => setInputVal(e.target.value)}
-        />
-      </label>
-      <button
-        onClick={() => {
-          setGreeting(inputVal);
-          setInputVal('');
-        }}
-      >
-        Save
-      </button>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label>
+          New Greeting:
+          <input
+            type="text"
+            value={inputVal}
+            onChange={(e) => setInputVal(e.target.value)}
+          />
+        </label>
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 }
